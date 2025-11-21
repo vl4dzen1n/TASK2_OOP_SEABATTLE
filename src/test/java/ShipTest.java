1f5783381a5fa38b017896d50207e49a8dbf9927
Vladislav <vladzenin89@yandex.ru>
2025-11-21 22:03:46 +0300

    Добавлены тесты для Ship

---
+++ src/test/java/ShipTest.java
@@ -0,0 +1,21 @@
+
+import models.Ship;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ShipTest {
+
+    @Test
+    void testShipHealthAndSunk() {
+        Ship ship = new Ship(3);
+        assertEquals(3, ship.getHealth());
+        assertFalse(ship.isSunk());
+
+        ship.hit();
+        assertEquals(2, ship.getHealth());
+
+        ship.hit();
+        ship.hit();
+        assertTrue(ship.isSunk());
+    }
+}
